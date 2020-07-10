2051efa179b4f11726db27e7142a25353a28a18e
ashish kumar singh <ashishkumarsinghh@outlook.com>
2020-07-10 15:34:51 +0530

    add okhttp client logging interceptor and timeout

--- app/src/main/java/com/example/learning_android_kotlin/RetrofitInstance.kt
+++ app/src/main/java/com/example/learning_android_kotlin/RetrofitInstance.kt
@@ -1,14 +1,31 @@
 package com.example.learning_android_kotlin
 
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttp
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class RetrofitInstance {
     companion object {
+
         private const val BASE_URL = "https://jsonplaceholder.typicode.com/"
+
+        private val interceptor = HttpLoggingInterceptor().apply{
+            this.level = HttpLoggingInterceptor.Level.BODY
+        }
+        val client = OkHttpClient.Builder().apply {
+            this.addInterceptor(interceptor)
+                .connectTimeout(10, TimeUnit.SECONDS)
+                .readTimeout(10, TimeUnit.SECONDS).
+                writeTimeout(10, TimeUnit.SECONDS)
+        }.build()
+
         fun getRetrofitInstance():Retrofit{
             return Retrofit.Builder().baseUrl(BASE_URL)
+                .client(client)
                 .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
                 .build()
         }
