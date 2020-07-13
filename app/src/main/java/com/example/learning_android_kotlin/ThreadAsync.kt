a5eff27bfb8bfced09f2f40101a0da26801a8f28
ashish kumar singh <ashishkumarsinghh@outlook.com>
2020-07-13 16:18:53 +0530

    communicating with UI from background threads

--- app/src/main/java/com/example/learning_android_kotlin/ThreadAsync.kt
+++ app/src/main/java/com/example/learning_android_kotlin/ThreadAsync.kt
@@ -1,13 +1,24 @@
 package com.example.learning_android_kotlin
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
 import android.util.Log
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.concurrent.thread
 
 class ThreadAsync : AppCompatActivity() {
 
+    private val handler = object:Handler(Looper.getMainLooper()){
+        override fun handleMessage(msg: Message) {
+            val bundle = msg.data
+            val uimsg = bundle.getString("UIMSG")
+            Log.d("THREAD2UI", uimsg)
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_thread_async)
@@ -23,5 +34,17 @@ class ThreadAsync : AppCompatActivity() {
                 Log.d("THREAD", "Logging from Thread. Loop value $i")
             }
         }.start()
+
+        thread(start=true){
+            for(i in 1..10){
+                val bundle = Bundle()
+                bundle.putString("UIMSG", "Logging from Kotlin thread(), Loop value $i")
+                Message().also{
+                    it.data = bundle
+                    handler.sendMessage(it)
+                }
+                //Log.d("KOTLINTHREAD", "Logging from Kotlin thread(), Loop value $i")
+            }
+        }
     }
 }
