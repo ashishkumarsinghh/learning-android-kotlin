b1ad2ff9b9db4dd586a98c585b2487aa1c72bf5d
ashish kumar singh <ashishkumarsinghh@outlook.com>
2020-07-13 16:04:32 +0530

    async operation with thread and handler in kotlin

--- app/src/main/java/com/example/learning_android_kotlin/ThreadAsync.kt
+++ app/src/main/java/com/example/learning_android_kotlin/ThreadAsync.kt
@@ -1,29 +1,27 @@
 package com.example.learning_android_kotlin
 
 import android.os.Bundle
-import android.os.Handler
 import android.util.Log
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 
-class HandlerAsync : AppCompatActivity() {
+class ThreadAsync : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_handler)
+        setContentView(R.layout.activity_thread_async)
         setSupportActionBar(findViewById(R.id.toolbar))
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
-    }
 
-    override fun onResume() {
-        super.onResume()
-        Handler().post { Log.d("HANDLER", "This comes from handler.")}
-        Handler().postDelayed({ Log.d("HANDLER", "This comes from handler.")}, 1000)
-        Handler().postAtTime({Log.d("HANDLER", "This one executes at this time.")}, System.currentTimeMillis()+3000)
+        Thread{
+            for(i in 1..10){
+                Log.d("THREAD", "Logging from Thread. Loop value $i")
+            }
+        }.start()
     }
 }
